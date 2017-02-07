c1395e772289c5c5cd0819a7c09c9a5077be53d2
Jared Swenson <jswen15@gmail.com>
2017-02-07 12:49:52 -0700

    side nav works, definitions done

--- hoppytaw.js
+++ hoppytaw.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
 	$('.modal').modal();
 	$('.carousel.carousel-slider').carousel({fullWidth: true});
 	$(".button-collapse").sideNav();
+	$('span').tooltip();
+	$('.modal-trigger').on('click', function () {
+		$('#mobile-demo').hide();
+		$('#mobile-demo').show();
+		$(".button-collapse").sideNav();
+	});
 });
